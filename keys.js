2ba7fe5eb257782f0b1842ecc42e2f33fbd7c8fe
rshah-nu <ronakshah2013@u.northwestern.edu>
2017-11-13 23:58:43 -0600

    Initial Commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('keys.js has been loaded');
+
+var twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+module.exports = twitterKeys;
